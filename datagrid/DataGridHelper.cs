3b516b991597548e2ed705dc2522021864e1d2cf
xy19xiaoyu <0714010877@163.com>
2017-12-15 17:29:33 +0800

    dagagrid

---
+++ datagrid/DataGridHelper.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Xml;
+
+namespace datagrid.UIHelper
+{
+    public static class DataGridHelper
+    {
+        /// <summary>         
+        /// Gets the visual child of an element         
+        /// </summary>         
+        /// <typeparam name="T">Expected type</typeparam>         
+        /// <param name="parent">The parent of the expected element</param>         
+        /// <returns>A visual child</returns>         
+        public static T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null)
+                {
+                    child = GetVisualChild<T>(v);
+                }
+                if (child != null)
+                {
+                    break;
+                }
+            }
+            return child;
+        }
+
+        /// <summary>
+        /// Gets the specified cell of the DataGrid
+        /// </summary>
+        /// <param name="grid">The DataGrid instance</param>
+        /// <param name="row">The row of the cell</param>
+        /// <param name="column">The column index of the cell</param>
+        /// <returns>A cell of the DataGrid</returns>
+        public static DataGridCell GetCell(this DataGrid grid, DataGridRow row, int column)
+        {
+            if (row != null)
+            {
+                DataGridCellsPresenter presenter = GetVisualChild<DataGridCellsPresenter>(row);
+                if (presenter == null)
+                {
+                    grid.ScrollIntoView(row, grid.Columns[column]);
+                    presenter = GetVisualChild<DataGridCellsPresenter>(row);
+                }
+                DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(column);
+                return cell;
+            }
+            return null;
+        }
+        /// <summary>
+        /// Gets the specified cell of the DataGrid
+        /// </summary>
+        /// <param name="grid">The DataGrid instance</param>
+        /// <param name="row">The row index of the cell</param>
+        /// <param name="column">The column index of the cell</param>
+        /// <returns>A cell of the DataGrid</returns>
+        public static DataGridCell GetCell(this DataGrid grid, int row, int column)
+        {
+            DataGridRow rowContainer = grid.GetRow(row);
+            return grid.GetCell(rowContainer, column);
+        }
+        /// <summary>
+        /// Gets the specified row of the DataGrid
+        /// </summary>
+        /// <param name="grid">The DataGrid instance</param>
+        /// <param name="index">The index of the row</param>
+        /// <returns>A row of the DataGrid</returns>
+        public static DataGridRow GetRow(this DataGrid grid, int index)
+        {
+
+            if (grid.Items.Count > index)
+            {
+                DataGridRow row = (DataGridRow)grid.ItemContainerGenerator.ContainerFromIndex(index);
+                if (row == null)
+                {
+                    // May be virtualized, bring into view and try again.
+                    grid.UpdateLayout();
+                    grid.ScrollIntoView(grid.Items[index]);
+                    row = (DataGridRow)grid.ItemContainerGenerator.ContainerFromIndex(index);
+                }
+                return row;
+            }
+            return null;
+
+        }
+        /// <summary>
+        /// Gets the selected row of the DataGrid
+        /// </summary>
+        /// <param name="grid">The DataGrid instance</param>
+        /// <returns></returns>
+        public static DataGridRow GetSelectedRow(this DataGrid grid)
+        {
+            return (DataGridRow)grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem);
+        }
+
+        public static DataGridCell GetCell(DataGridCellInfo dataGridCellInfo)
+        {
+            if (!dataGridCellInfo.IsValid)
+            {
+                return null;
+            }
+
+            var cellContent = dataGridCellInfo.Column.GetCellContent(dataGridCellInfo.Item);
+            if (cellContent != null)
+            {
+                return (DataGridCell)cellContent.Parent;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static int GetRowIndex(DataGridCell dataGridCell)
+        {
+            // Use reflection to get DataGridCell.RowDataItem property value.
+            PropertyInfo rowDataItemProperty = dataGridCell.GetType().GetProperty("RowDataItem", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            DataGrid dataGrid = GetDataGridFromChild(dataGridCell);
+
+            return dataGrid.Items.IndexOf(rowDataItemProperty.GetValue(dataGridCell, null));
+        }
+
+        public static DataGrid GetDataGridFromChild(DependencyObject dataGridPart)
+        {
+            if (VisualTreeHelper.GetParent(dataGridPart) == null)
+            {
+                throw new NullReferenceException("Control is null.");
+            }
+            if (VisualTreeHelper.GetParent(dataGridPart) is DataGrid)
+            {
+                return (DataGrid)VisualTreeHelper.GetParent(dataGridPart);
+            }
+            else
+            {
+                return GetDataGridFromChild(VisualTreeHelper.GetParent(dataGridPart));
+            }
+        }
+
+        public static DataGridTemplateColumn GenTemplateColumn(string strHeader, string strElement, double nWidth, DataGridLengthUnitType lenType)
+        {
+            DataGridTemplateColumn item = new DataGridTemplateColumn();
+            item.Header = strHeader;
+            item.Width = new DataGridLength(nWidth, lenType);
+            item.CanUserReorder = false;
+            item.CanUserResize = false;
+            item.CanUserSort = false;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<DataTemplate");
+            sb.Append(" xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'");
+            sb.Append(" xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>");
+            sb.Append(strElement);
+            sb.Append("</DataTemplate>");
+
+            StringReader reader = new StringReader(sb.ToString());
+            XmlTextReader xmlReader = new XmlTextReader(reader);
+            item.CellTemplate = (DataTemplate)XamlReader.Load(xmlReader);
+            return item;
+        }
+
+        public static DataGridTemplateColumn GenTemplateColumnAllText(string strHeader, string strHeaderElement, string strCellElement, double nWidth, DataGridLengthUnitType lenType)
+        {
+            DataGridTemplateColumn item = new DataGridTemplateColumn();
+            item.Header = strHeader;
+            item.Width = new DataGridLength(nWidth, lenType);
+            item.CanUserReorder = false;
+            item.CanUserResize = false;
+            item.CanUserSort = false;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<DataTemplate");
+            sb.Append(" xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'");
+            sb.Append(" xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>");
+            sb.Append(strCellElement);
+            sb.Append("</DataTemplate>");
+
+            StringReader reader = new StringReader(sb.ToString());
+            XmlTextReader xmlReader = new XmlTextReader(reader);
+            item.CellTemplate = (DataTemplate)XamlReader.Load(xmlReader);
+
+            sb.Clear();
+            sb.Append("<DataTemplate");
+            sb.Append(" xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'");
+            sb.Append(" xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'>");
+            sb.Append(strHeaderElement);
+            sb.Append("</DataTemplate>");
+
+            reader = new StringReader(sb.ToString());
+            xmlReader = new XmlTextReader(reader);
+            item.HeaderTemplate = (DataTemplate)XamlReader.Load(xmlReader);
+            return item;
+        }
+    }
+}
---
+++ datagrid/MainWindow.xaml.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using datagrid.UIHelper;
+
+namespace datagrid
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Test> GridSource = new ObservableCollection<Test>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private Test SelectItem;
+        private string Colhead { get; set; }
+
+        private int RowIndex { get; set; } = 0;
+        private int ColIndex { get; set; } = 0;
+
+        private DataGridCell Cell { get; set; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AddNewRow();
+            FocusLastRowFirstCell();
+
+        }
+
+        private void AddNewRow()
+        {
+            bool boAdd = true;
+            if (GridSource.Count > 0)
+            {
+                foreach (Test item in GridSource)
+                {
+                    if (item.CategoryName == null || String.IsNullOrEmpty(item.CategoryName))
+                    {
+                        boAdd = false;
+                    }
+                    else
+                    {
+                        item.RowFunction = 1;
+                    }
+                }
+            }
+
+            if (boAdd)
+            {
+                Test newItem = new Test();
+                GridSource.Add(newItem);
+                this.dataGrid.ItemsSource = GridSource;
+                this.dataGrid.ScrollIntoView(newItem);
+            }
+        }
+
+        private void FocusLastRowFirstCell()
+        {
+            #region//选中最后一行第一个单元格
+            this.dataGrid.ScrollIntoView(GridSource[GridSource.Count - 1]);
+            this.dataGrid.CurrentItem = GridSource[GridSource.Count - 1];
+            DataGridCell cell = this.dataGrid.GetCell(GridSource.Count - 1, 0);
+            cell.IsSelected = true;
+            cell.Focus();
+            #endregion
+        }
+
+        private void SelectNext(int row = -1, int Col = -1)
+        {
+
+            if (row == -1 || Col == -1)
+            {
+                if (dataGrid.CurrentItem == null) return;
+                row = dataGrid.Items.IndexOf(dataGrid.CurrentItem);
+                Col = dataGrid.Columns.IndexOf(dataGrid.CurrentColumn);
+                if (dataGrid.CurrentCell.Column.Header.ToString() == "服务")
+                {
+                    RowIndex = row + 1;
+                    ColIndex = 0;
+                }
+                else
+                {
+                    RowIndex = row;
+                    ColIndex = Col + 1;
+                }
+            }
+            else
+            {
+                RowIndex = row;
+                ColIndex = Col;
+            }
+
+
+            if (dataGrid.Items.Count - 1 <= row)
+            {
+                AddNewRow();
+            }
+            Cell = DataGridHelper.GetCell(dataGrid, RowIndex, ColIndex);
+            if (Cell != null)
+            {
+                Cell.IsSelected = true;
+                Cell.Focus();
+                dataGrid.BeginEdit();
+
+                SelectItem = dataGrid.CurrentItem as Test;
+
+                ppp.PlacementTarget = Cell;
+                ppp.IsOpen = false;
+                ppp.IsOpen = true;
+
+            }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectItem == null) return;
+            SelectItem.CategoryName = "test";
+            if (Colhead == "服务")
+            {
+                RowIndex++;
+                ColIndex = 0;
+            }
+            else
+            {
+                ColIndex++;
+            }
+            SelectNext(RowIndex, ColIndex);
+
+        }
+
+        private void dataGrid_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (SelectItem == null) return;
+
+                if (Colhead == "品类")
+                {
+                    if (string.IsNullOrEmpty(SelectItem.CategoryName)) return;
+                    AddNewRow();
+                }
+                SelectNext();
+            }
+        }
+
+
+
+        private void dataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
+            if (dataGrid.SelectedCells.Count == 0) return;
+            Colhead = dataGrid.SelectedCells[0].Column.Header.ToString();
+
+            if (dataGrid.CurrentItem == null) return;
+            SelectItem = this.dataGrid.CurrentItem as Test;
+            RowIndex = dataGrid.Items.IndexOf(dataGrid.SelectedCells[0].Item);
+            ColIndex = dataGrid.Columns.IndexOf(dataGrid.SelectedCells[0].Column);
+            if (ColIndex < 0) ColIndex = 0;
+            dataGrid.BeginEdit();
+
+
+            Cell = DataGridHelper.GetCell(dataGrid, RowIndex, ColIndex);
+            //ppp.PlacementTarget = Cell;
+            //ppp.IsOpen = false;
+            //ppp.IsOpen = true;
+        }
+
+        private void dataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (SelectItem == null) return;
+            string text = (sender as Button).Content.ToString();
+            switch (Colhead)
+            {
+
+                case "品类":
+                    SelectItem.CategoryName = text;
+                    break;
+                case "条码":
+                    SelectItem.SN = text;
+                    break;
+                case "名称":
+                    SelectItem.Name = text;
+                    break;
+                case "颜色":
+                    SelectItem.Color = text;
+                    break;
+                case "服务":
+                    SelectItem.Service = text;
+                    break;
+            }
+         
+            if (Colhead == "服务")
+            {
+                RowIndex++;
+                ColIndex = 0;
+            }
+            else
+            {
+                ColIndex++;
+            }
+            ppp.IsOpen = false;
+            SelectNext(RowIndex, ColIndex);
+
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            Test t = btn.DataContext as Test;
+            if (t == null) return;
+            GridSource.Remove(t);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            Test t = btn.DataContext as Test;
+            if (t == null) return;
+            GridSource.Add(new Test() { Name = t.Name, CategoryName = t.CategoryName, Color = t.Color, Brand = t.Brand, Service = t.Service });
+        }
+    }
+
+    public class Test : INotifyPropertyChanged
+    {
+        public string _CategoryName;
+        public string CategoryName
+        {
+            get { return _CategoryName; }
+            set
+            {
+                _CategoryName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CategoryName"));
+            }
+        }
+
+        public string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public string _Color;
+        public string Color
+        {
+            get { return _Color; }
+            set
+            {
+                _Color = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Color"));
+            }
+        }
+
+        public string _Brand;
+        public string Brand
+        {
+            get { return _Brand; }
+            set
+            {
+                _Brand = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Brand"));
+            }
+        }
+
+        public string _Service;
+        public string Service
+        {
+            get { return _Service; }
+            set
+            {
+                _Service = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Service"));
+            }
+        }
+
+        public string _SN;
+        public string SN
+        {
+            get { return _SN; }
+            set
+            {
+                _SN = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SN"));
+            }
+        }
+
+        private int _RowFunction;
+
+        public int RowFunction
+        {
+            get { return _RowFunction; }
+            set
+            {
+                _RowFunction = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RowFunction"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
